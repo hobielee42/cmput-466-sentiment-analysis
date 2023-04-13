a3770568f1adb7e1c8096d7d60c8ec8df537e56d
Hobie Li <hobbylee6@gmail.com>
2023-04-13 02:45:01 -0600

    init

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import os
+import shutil
+
+import tensorflow as tf
 from keras.utils import text_dataset_from_directory
+
+
+def get_data():
+    url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
+    dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+    train_dir = os.path.join(dataset_dir, 'train')
+    os.listdir(train_dir)
+    remove_dir = os.path.join(train_dir, 'unsup')
+    shutil.rmtree(remove_dir)
+
 
 raw_train_ds = text_dataset_from_directory('aclImdb/train', batch_size=32, validation_split=0.2, subset='training',
                                            seed=42)
