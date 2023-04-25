31922ab1c97ce3cda329f2186dcb36d756ff7361
Hobie Li <hobbylee6@gmail.com>
2023-04-25 05:54:14 -0600

    rnn

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_data():
     """
     url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
     dataset = tf.keras.utils.get_file(
-        "aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
+            "aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
     dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
     train_dir = os.path.join(dataset_dir, 'train')
     os.listdir(train_dir)
@@ -68,6 +68,19 @@ def cnn():
     return model
 
 
+def rnn():
+    model = tf.keras.Sequential([
+        vectorize_layer,
+        tf.keras.layers.Embedding(vocab_size, 8, mask_zero=True),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(8)),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')])
+    model.compile(loss="binary_crossentropy",
+                  optimizer='adam',
+                  metrics=tf.metrics.BinaryAccuracy())
+    return model
+
+
 # main
 raw_train_ds = text_dataset_from_directory('aclImdb/train',
                                            batch_size=batch_size,
@@ -85,12 +98,11 @@ raw_test_ds = tf.keras.utils.text_dataset_from_directory('aclImdb/test',
                                                          batch_size=batch_size)
 
 vectorize_layer = TextVectorization(
-    standardize=custom_standardization,
-    output_mode='int')
+        standardize=custom_standardization,
+        output_mode='int')
 
 vectorize_layer.adapt(raw_train_ds.map(lambda x, y: x))
 vocab_size = vectorize_layer.vocabulary_size()
-
 
 model1 = nn()
 model1.summary()
@@ -100,14 +112,14 @@ model2.summary()
 
 epochs = 5
 model1.fit(
-    raw_train_ds,
-    validation_data=raw_val_ds,
-    epochs=epochs)
+        raw_train_ds,
+        validation_data=raw_val_ds,
+        epochs=epochs)
 
 model2.fit(
-    raw_train_ds,
-    validation_data=raw_val_ds,
-    epochs=epochs)
+        raw_train_ds,
+        validation_data=raw_val_ds,
+        epochs=epochs)
 
 loss1, accuracy1 = model1.evaluate(raw_test_ds)
 
